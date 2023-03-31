d673ff475572b99358aa0c0952d4bc09f2ac2775
Voron4ikhin <voron.ilya16@gmail.com>
2023-03-31 17:00:20 +0300

    first simple examples

---
+++ src/payments/controllers/payments/payments.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Controller('payments')
+export class PaymentsController {
+
+@Get()
+getPayments(@Req() request: Request, @Res() response: Response){
+    
+    const  {count, page} = request.query;
+    if(!count || !page){
+        response
+        .status(400)
+        .send({msg: 'Missing count or page query parametr'})
+    } else{
+        response.send(200);
+    }
+
+}
+}
+
